001bd9f40cf9c6023e44fd25f8766d389f7e7e2b
mahmuda-bjitgroup <mahmuda.keya@bjitgroup.com>
2023-08-14 14:38:04 +0600

    Edited resource.tf and provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,23 +1,4 @@
-# with aws credentials exposed
-# provider "aws" {
-#   region     = "us-east-1"
-#   access_key = "AKIAZITYUD2SS3VVMLHU"
-#   secret_key = "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"
-# }
-
 # with aws cli
 provider "aws" {
 region     = "us-east-1"
 }
-
-# terraform {
-#     required_providers {
-#       aws = {
-#         source = "hashicorp/aws"
-#         version = ">5.11.0"
-#       }
-#     }   
-# }
-
-# with aws environment variable
-# provider "aws" {}
